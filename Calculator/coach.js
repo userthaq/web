3be90f14b61f716c445d249314840d12a2ba12e8
Thaqif <debug.thaqif@gmail.com>
2024-03-09 17:46:38 +0800

    update calc

--- Calculator/coach.js
+++ Calculator/coach.js
@@ -1,5 +1,8 @@
-var firstValue = 0;
+var arrNumber = [];
+var arrOperator = [];
+var calcHistory = [];
 var enteredNumber = "";
+var firstValue = 0;
 
 function numberClicked(num) {
   var display = document.querySelector("#calc_display");
@@ -15,12 +18,16 @@ function numberClicked(num) {
 function clearClicked() {
   var display = document.querySelector("#calc_display");
   display.innerHTML = 0;
+  arrOperator = [];
+  arrNumber = [];
 }
 
 function operatorClicked(char) {
   var display = document.querySelector("#calc_display");
   if (display.innerHTML == "0") {
   } else {
+    arrNumber.push(enteredNumber);
+    arrOperator.push(char);
     firstValue = Number(enteredNumber);
     enteredNumber = "";
     display.innerHTML += char;
@@ -29,13 +36,29 @@ function operatorClicked(char) {
 
 function resultClicked() {
   var display = document.querySelector("#calc_display");
-  if (display.innerHTML.includes("+")) {
-    display.innerHTML = Number(firstValue) + Number(enteredNumber);
-  } else if (display.innerHTML.includes("-")) {
-    display.innerHTML = Number(firstValue) - Number(enteredNumber);
-  } else if (display.innerHTML.includes("x")) {
-    display.innerHTML = Number(firstValue) * Number(enteredNumber);
-  } else if (display.innerHTML.includes("÷")) {
-    display.innerHTML = Number(firstValue) / Number(enteredNumber);
+  arrNumber.push(enteredNumber);
+  var total = arrNumber[0];
+  for (let i = 0; i < arrOperator.length; i++) {
+    total = operation(total, arrNumber[i + 1], arrOperator[i]);
+  }
+  display.innerHTML = total;
+  calcHistory.push(total);
+}
+
+function operation(num1, num2, operator) {
+  if (operator == "+") {
+    return Number(num1) + Number(num2);
+  } else if (operator == "-") {
+    return Number(num1) - Number(num2);
+  } else if (operator == "x") {
+    return Number(num1) * Number(num2);
+  } else if (operator == "÷") {
+    return Number(num1) / Number(num2);
+  }
+}
+
+function history() {
+  for (let i = 0; i < calcHistory.length; i++) {
+    historyDisplay.innerHTML = calcHistory[i];
   }
 }
